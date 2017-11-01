2a9d56756ab06a6f47bbce44b8c45c819d56f7d8
michael.englert <michi.eng@gmail.com>
2017-11-01 15:03:17 +0100

    adjusted method/variable access added suppression

--- src/main/java/com/appdynamics/monitors/azure/AzureRestOperation.java
+++ src/main/java/com/appdynamics/monitors/azure/AzureRestOperation.java
@@ -4,14 +4,13 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.microsoft.aad.adal4j.AuthenticationResult;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class AzureRestOperation {
+class AzureRestOperation {
     public static JsonNode doGet(AuthenticationResult azureAuth, URL url) throws IOException {
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setDoOutput(true);
@@ -21,6 +20,7 @@ public class AzureRestOperation {
         BufferedReader br = new BufferedReader(new InputStreamReader(
                 (conn.getInputStream())));
         String response = "";
+        //noinspection StatementWithEmptyBody
         for (String line; (line = br.readLine()) != null; response += line);
         conn.disconnect();
         ObjectMapper objectMapper = new ObjectMapper();
