cb51db46b8c6b4dc0a401ff6097d128c0fccc051
michael.englert <michi.eng@gmail.com>
2017-11-01 15:10:35 +0100

    added encryption test case

--- src/test/java/com/appdynamics/monitors/azure/AzureMonitorTest.java
+++ src/test/java/com/appdynamics/monitors/azure/AzureMonitorTest.java
@@ -2,7 +2,6 @@ package com.appdynamics.monitors.azure;
 
 import com.appdynamics.extensions.conf.MonitorConfiguration;
 import com.appdynamics.extensions.util.MetricWriteHelper;
-import com.appdynamics.monitors.azure.config.ConfigTask;
 import com.appdynamics.monitors.azure.config.Globals;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
@@ -19,26 +18,64 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
-
 import static org.junit.Assert.assertTrue;
 
 public class AzureMonitorTest {
 
     @Test
-    public void testAzureMonitor() throws TaskExecutionException, InterruptedException {
+    public void testAzureMonitor(){
         Map<String, String> taskArgs = new HashMap<String, String>();
         taskArgs.put("config-file", "src/test/resources/conf/integration-test-config.yml");
+        try {
+            testAzureMonitorRun(taskArgs);
+        } catch (TaskExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
+    @Test
+    public void testAzureMonitorWithEncryption(){
+        Map<String, String> taskArgs = new HashMap<String, String>();
+        taskArgs.put("config-file", "src/test/resources/conf/integration-test-encrypted-config.yml");
+        try {
+            testAzureMonitorRun(taskArgs);
+        } catch (TaskExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+   @Test
+   public void testAzureMonitorTask(){
+       try {
+           testAzureMonitorTaskRun("src/test/resources/conf/integration-test-config.yml");
+       } catch (Exception e) {
+           e.printStackTrace();
+       }
+   }
+
+    @Test
+    public void testAzureMonitorTaskWithEncryption(){
+        try {
+            testAzureMonitorTaskRun("src/test/resources/conf/integration-test-encrypted-config.yml");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void testAzureMonitorRun(Map<String, String> taskArgs) throws TaskExecutionException, InterruptedException {
         TaskOutput result = new AzureMonitor().execute(taskArgs, null);
         assertTrue(result.getStatusMessage().contains("Metric Upload Complete"));
     }
 
-    @Test
-    public void testAzureMonitorTask() throws Exception {
+    public void testAzureMonitorTaskRun(String configYml) throws Exception {
         MetricWriteHelper writer = Mockito.mock(MetricWriteHelper.class);
         Runnable runner = Mockito.mock(Runnable.class);
-        MonitorConfiguration conf = new MonitorConfiguration("Custom Metrics|AzureMonitor|", runner, writer);
-        conf.setConfigYml("src/test/resources/conf/integration-test-config.yml");
+        MonitorConfiguration conf = new MonitorConfiguration(Globals.defaultMetricPrefix, runner, writer);
+        conf.setConfigYml(configYml);
         Mockito.doAnswer(new Answer() {
             public Object answer(InvocationOnMock invocationOnMock) throws Throwable {
                 Object[] args = invocationOnMock.getArguments();
@@ -50,11 +87,12 @@ public class AzureMonitorTest {
 
         AuthenticationResult azureAuth = AzureAuth.getAzureAuth(
                 (String) conf.getConfigYml().get(Globals.clientId),
-                (String) conf.getConfigYml().get(Globals.clientKey),
+                Utilities.getClientKey(conf.getConfigYml()),
                 (String) conf.getConfigYml().get(Globals.tenantId));
 
+        //noinspection unchecked
         List<Map> filters = (List<Map>) conf.getConfigYml().get(Globals.azureApiFilter);
-        String filterUrl = ConfigTask.getFilters(filters);
+        String filterUrl = Utilities.getFilters(filters);
         URL url = new URL(Globals.azureEndpoint + Globals.azureApiSubscriptions + conf.getConfigYml().get(Globals.subscriptionId) + Globals.azureApiResources +
                 "?" + Globals.azureApiVersion + "=" + conf.getConfigYml().get(Globals.azureApiVersion) +
                 filterUrl);
