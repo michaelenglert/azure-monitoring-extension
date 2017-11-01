04ade1881e4b0d1cab92042b92b6ecaa0b09dc62
michael.englert <michi.eng@gmail.com>
2017-11-01 14:59:27 +0100

    replaced ConfigTask with Utilities

--- src/main/java/com/appdynamics/monitors/azure/Utilities.java
+++ src/main/java/com/appdynamics/monitors/azure/Utilities.java
@@ -1,12 +1,16 @@
-package com.appdynamics.monitors.azure.config;
+package com.appdynamics.monitors.azure;
 
+import com.appdynamics.extensions.crypto.CryptoUtil;
+import com.appdynamics.monitors.azure.config.Globals;
+import com.google.common.base.Strings;
+import com.google.common.collect.Maps;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-public class ConfigTask {
+public class Utilities {
     public static String getFilters(List<Map> filters) throws UnsupportedEncodingException {
         StringBuilder filterUrl = null;
         if (filters != null && !filters.isEmpty()) {
@@ -24,5 +28,21 @@ public class ConfigTask {
             }
         }
         return filterUrl != null ? filterUrl.toString() : null;
+    }
+
+    static String getClientKey(Map<String, ?> config) {
+        String clientKey = (String) config.get(Globals.clientKey);
+        if (!Strings.isNullOrEmpty(clientKey)) {
+            return clientKey;
+        }
+        String encryptionKey = (String) config.get(Globals.encryptionKey);
+        String encryptedClientKey = (String) config.get(Globals.encryptedClientKey);
+        if (!Strings.isNullOrEmpty(encryptionKey) && !Strings.isNullOrEmpty(encryptedClientKey)) {
+            java.util.Map<String, String> cryptoMap = Maps.newHashMap();
+            cryptoMap.put(Globals.passwordEncrypted, encryptedClientKey);
+            cryptoMap.put(Globals.encryptionKey, encryptionKey);
+            return CryptoUtil.getPassword(cryptoMap);
+        }
+        return null;
     }
 }
