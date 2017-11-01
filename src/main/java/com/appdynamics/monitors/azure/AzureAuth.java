a782daf17466d94c7104100d666718c96184b8b1
michael.englert <michi.eng@gmail.com>
2017-11-01 15:00:46 +0100

    adjusted variable name to be consistent

--- src/main/java/com/appdynamics/monitors/azure/AzureAuth.java
+++ src/main/java/com/appdynamics/monitors/azure/AzureAuth.java
@@ -4,20 +4,19 @@ import com.appdynamics.monitors.azure.config.Globals;
 import com.microsoft.aad.adal4j.AuthenticationContext;
 import com.microsoft.aad.adal4j.AuthenticationResult;
 import com.microsoft.aad.adal4j.ClientCredential;
-
 import java.net.MalformedURLException;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-public class AzureAuth {
-    public static AuthenticationResult getAzureAuth (String clientId, String clientSecret,String tenantId) throws MalformedURLException, ExecutionException, InterruptedException {
+class AzureAuth {
+    public static AuthenticationResult getAzureAuth (String clientId, String clientKey,String tenantId) throws MalformedURLException, ExecutionException, InterruptedException {
         ExecutorService service = Executors.newFixedThreadPool(1);
         AuthenticationContext context;
         AuthenticationResult result;
         context = new AuthenticationContext(Globals.azureAuthEndpoint + tenantId, false, service);
-        ClientCredential cred = new ClientCredential(clientId, clientSecret);
+        ClientCredential cred = new ClientCredential(clientId, clientKey);
         Future<AuthenticationResult> future = context.acquireToken(Globals.azureEndpoint + "/", cred, null);
         result = future.get();
 
