3635878b7f3f19c2bb8d785dbde42a89578a7c6f
Isi Ramirez <isiryder@gmail.com>
2018-08-06 11:33:14 +0200

    Refactor metrics / Add azureMetricCallCount and azureMetricDefinitionsCallCount

--- src/main/java/com/appdynamics/monitors/azure/metrics/AzureMetrics.java
+++ src/main/java/com/appdynamics/monitors/azure/metrics/AzureMetrics.java
@@ -23,10 +23,15 @@ import org.slf4j.LoggerFactory;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @SuppressWarnings("unchecked")
 public class AzureMetrics implements AMonitorTaskRunnable {
     private static final org.slf4j.Logger logger = LoggerFactory.getLogger(AzureMetrics.class);
+
+    public final AtomicInteger azureMetricsCallCount = new AtomicInteger(0);
+    public final AtomicInteger azureMetricDefinitionsCallCount = new AtomicInteger(0);
+
     private final Map<String, ?> resourceFilter;
     private final String currentResourceGroupFilter;
     private final String currentResourceFilter;
@@ -37,6 +42,10 @@ public class AzureMetrics implements AMonitorTaskRunnable {
     private final MetricWriteHelper metricWriteHelper;
     private final Azure azure;
     private final String metricPrefix;
+
+	private List<Metric> finalMetricList = Lists.newArrayList();
+
+	private DateTime recordDateTime = DateTime.now();
 
     public AzureMetrics(Map<String, ?> resourceFilter,
                         String currentResourceGroupFilter,
@@ -78,8 +87,6 @@ public class AzureMetrics implements AMonitorTaskRunnable {
         }
     }
     private void runTask(){
-        List<Metric> finalMetricList = Lists.newArrayList();
-        DateTime recordDateTime = DateTime.now();
         if (resource.name().matches(currentResourceFilter) &&
                 resource.resourceType().matches(currentResourceTypeFilter) &&
                 resource.resourceGroupName().matches(currentResourceGroupFilter)) {
@@ -92,77 +99,11 @@ public class AzureMetrics implements AMonitorTaskRunnable {
                         currentResourceTypeFilter,
                         currentResourceGroupFilter);
             }
+            azureMetricDefinitionsCallCount.incrementAndGet();
             List<MetricDefinition> resourceMetrics = azure.metricDefinitions().listByResource(resource.id());
+            logger.info("fos: " + resourceMetrics.toString() + " " + resourceFilter.toString());
             List<Map<String, String>> metricFilters = (List<Map<String, String>>) resourceFilter.get("metrics");
-
-            for (Map<String, String> metricFilter : metricFilters) {
-                for (MetricDefinition resourceMetric : resourceMetrics) {
-                    String currentMetricFilter = metricFilter.get("metric");
-                    if (resourceMetric.isDimensionRequired()) {
-                        if (logger.isDebugEnabled()) {
-                            logger.debug("Resource Metric {} needs Dimensions. This is currently not supported",
-                                    resourceMetric.id());
-                        }
-                    } else if (resourceMetric.name().value().matches(currentMetricFilter)) {
-                        MetricCollection metricCollection = resourceMetric.defineQuery().startingFrom(recordDateTime.minusMinutes(2)).endsBefore(recordDateTime).execute();
-                        String metricAggregation = resourceMetric.primaryAggregationType().toString();
-                        String metricName = resourceMetric.name().value();
-                        String metricValue = null;
-                        String metricPath = metricPrefix +
-                                Constants.METRIC_SEPARATOR +
-                                subscriptionName +
-                                Constants.METRIC_SEPARATOR +
-                                resource.resourceGroupName() +
-                                Constants.METRIC_SEPARATOR +
-                                resource.resourceType() +
-                                Constants.METRIC_SEPARATOR +
-                                resource.name() +
-                                Constants.METRIC_SEPARATOR;
-                        switch (metricAggregation) {
-                            case "Count":
-                                metricValue = String.valueOf(metricCollection.metrics().get(0).timeseries().get(0).data().get(0).count());
-                                break;
-                            case "Average":
-                                metricValue = String.valueOf(metricCollection.metrics().get(0).timeseries().get(0).data().get(0).average());
-                                break;
-                            case "Total":
-                                metricValue = String.valueOf(metricCollection.metrics().get(0).timeseries().get(0).data().get(0).total());
-                                break;
-                            case "Maximum":
-                                metricValue = String.valueOf(metricCollection.metrics().get(0).timeseries().get(0).data().get(0).maximum());
-                                break;
-                            default:
-                                logger.info("Not Reporting Metric {} for Resource {} as the aggregation type is not supported",
-                                        metricName,
-                                        resource.name());
-                                break;
-                        }
-                        assert metricValue != null;
-                        if (metricValue.isEmpty() || metricValue.equals("0.0") || metricValue.equals("null")) {
-                            logger.debug("Ignoring Metric {} for Resource {} as it is null or empty",
-                                    metricName,
-                                    resource.name(),
-                                    metricValue);
-                        } else {
-                            Metric metric = new Metric(metricName, metricValue, metricPath + metricName);
-                            if (logger.isDebugEnabled()) {
-                                logger.debug("Reporting Metric {} for Resource {} with value {}",
-                                        metricName,
-                                        resource.name(),
-                                        metricValue);
-                            }
-                            finalMetricList.add(metric);
-                        }
-                    } else {
-                        if (logger.isDebugEnabled()) {
-                            logger.debug("Not Reporting Metric {} for Resource {} as it is filtered by {}",
-                                    resourceMetric.name().value(),
-                                    resource.name(),
-                                    currentMetricFilter);
-                        }
-                    }
-                }
-            }
+            getResourceMetrics(resourceMetrics, metricFilters);
         } else {
             if (logger.isDebugEnabled()) {
                 logger.debug("Skipping Resource {} of Type {} in Group {} because of Resource Filter {} of Type Filter {} in Group Filter {}",
@@ -180,6 +121,80 @@ public class AzureMetrics implements AMonitorTaskRunnable {
             }
         } else {
             metricWriteHelper.transformAndPrintMetrics(finalMetricList);
+        }
+        logger.debug("azureMetricDefinitionsCallCount {}: {}", resource.id(), azureMetricDefinitionsCallCount);
+        logger.debug("azureMetricsCallCount: " + azureMetricsCallCount);
+    }
+
+	private void getResourceMetrics(List<MetricDefinition> resourceMetrics, List<Map<String, String>> metricFilters) {
+        for (Map<String, String> metricFilter : metricFilters) {
+            for (MetricDefinition resourceMetric : resourceMetrics) {
+				String currentMetricFilter = metricFilter.get("metric");
+				if (resourceMetric.isDimensionRequired()) {
+				    if (logger.isDebugEnabled()) {
+				        logger.debug("Resource Metric {} needs Dimensions. This is currently not supported",
+				                resourceMetric.id());
+				    }
+				} else if (resourceMetric.name().value().matches(currentMetricFilter)) {
+					azureMetricsCallCount.incrementAndGet();
+				    MetricCollection metricCollection = resourceMetric.defineQuery().startingFrom(recordDateTime.minusMinutes(2)).endsBefore(recordDateTime).execute();
+				    String metricAggregation = resourceMetric.primaryAggregationType().toString();
+				    String metricName = resourceMetric.name().value();
+				    String metricValue = null;
+				    String metricPath = metricPrefix +
+				            Constants.METRIC_SEPARATOR +
+				            subscriptionName +
+				            Constants.METRIC_SEPARATOR +
+				            resource.resourceGroupName() +
+				            Constants.METRIC_SEPARATOR +
+				            resource.resourceType() +
+				            Constants.METRIC_SEPARATOR +
+				            resource.name() +
+				            Constants.METRIC_SEPARATOR;
+				    switch (metricAggregation) {
+				        case "Count":
+				            metricValue = String.valueOf(metricCollection.metrics().get(0).timeseries().get(0).data().get(0).count());
+				            break;
+				        case "Average":
+				            metricValue = String.valueOf(metricCollection.metrics().get(0).timeseries().get(0).data().get(0).average());
+				            break;
+				        case "Total":
+				            metricValue = String.valueOf(metricCollection.metrics().get(0).timeseries().get(0).data().get(0).total());
+				            break;
+				        case "Maximum":
+				            metricValue = String.valueOf(metricCollection.metrics().get(0).timeseries().get(0).data().get(0).maximum());
+				            break;
+				        default:
+				            logger.info("Not Reporting Metric {} for Resource {} as the aggregation type is not supported",
+				                    metricName,
+				                    resource.name());
+				            break;
+				    }
+				    assert metricValue != null;
+				    if (metricValue.isEmpty() || metricValue.equals("0.0") || metricValue.equals("null")) {
+				        logger.debug("Ignoring Metric {} for Resource {} as it is null or empty",
+				                metricName,
+				                resource.name(),
+				                metricValue);
+				    } else {
+				        Metric metric = new Metric(metricName, metricValue, metricPath + metricName);
+				        if (logger.isDebugEnabled()) {
+				            logger.debug("Reporting Metric {} for Resource {} with value {}",
+				                    metricName,
+				                    resource.name(),
+				                    metricValue);
+				        }
+				        finalMetricList.add(metric);
+				    }
+				} else {
+				    if (logger.isDebugEnabled()) {
+				        logger.debug("Not Reporting Metric {} for Resource {} as it is filtered by {}",
+				                resourceMetric.name().value(),
+				                resource.name(),
+				                currentMetricFilter);
+				    }
+				}
+			}
         }
     }
 }
